f38bd2c3667dfc4c59cf98465ca8bfb2a2e31781
Priyu2 <25priyakadu@gmail.com>
2017-12-19 15:26:33 +0530

    my first demo

---
+++ EclipsWorkspace/practice1/src/demo1/Program1.java
@@ -0,0 +1,12 @@
+package demo1;
+
+public class Program1 {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("welcome geecon");
+	System.out.println("my first amotomation script");
+
+	}
+
+}
